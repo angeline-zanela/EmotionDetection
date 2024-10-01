012e65d10c74d29156d57b94794027dd24907603
angeline-zanela <91962558+angeline-zanela@users.noreply.github.com>
2024-10-01 17:56:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input, Concat
 from tensorflow.keras.applications import VGG16
 from PIL import Image
 
-# Carrega o classificador Haar Cascade pré-treinado para detecção de rostos
+# Foi usado o classificador Haar Cascade pré-treinado para detecção de rostos.
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Carrega o preditor de landmarks faciais da dlib
+# Usado esse arquivo para carregar os pontos do rosto
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Inicializar MediaPipe para detecção de mãos
+# Usado o mediapipe para reconhecer os pontos das mãos
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, 
                        min_detection_confidence=0.7, min_tracking_confidence=0.7)
 
-# Função para criar o modelo de emoções faciais com VGG16 e landmarks faciais
+# Cria o modelo, mas ele já está treinado
 def create_face_model(input_shape_image, input_shape_landmarks, num_classes):
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=input_shape_image)
     base_model.trainable = True
@@ -40,27 +40,25 @@ def create_face_model(input_shape_image, input_shape_landmarks, num_classes):
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Função para detectar rostos e landmarks usando dlib
+# Essa função detecta on pontos com a dlib
 def detect_face_and_landmarks(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
     if len(faces) > 0:
-        x, y, w, h = faces[0]  # Assume que há apenas um rosto e pega o primeiro
+        x, y, w, h = faces[0]  # Pega apenas 1 rosto na imagem
         face_region = image[y:y+h, x:x+w]  # Recorta a região do rosto
-        face_region_gray = gray[y:y+h, x:x+w]  # Região do rosto em escala de cinza
-        
-        # Detecta landmarks na região do rosto usando dlib
+        face_region_gray = gray[y:y+h, x:x+w]  # Região do rosto em escala de cinza para melhorar a classificação
         rect = dlib.rectangle(x, y, x+w, y+h)
         landmarks = predictor(face_region_gray, rect)
         landmark_points = []
-        for i in range(0, 68):  # 68 landmarks fornecidos pelo dlib
+        for i in range(0, 68):  # Usa os 68 landmarks fornecidos pelo dlib no .dat
             landmark_points.append([landmarks.part(i).x, landmarks.part(i).y])
         return face_region, np.array(landmark_points).flatten()
     else:
-        return None, None  # Retorna None se nenhum rosto for detectado
+        return None, None  # Retorna nada se nenhum rosto for detectado
 
-# Função para verificar a validade dos landmarks
+# Função que verifica a validade dos landmarks
 def are_landmarks_valid(landmarks, image_shape):
     if landmarks is None or len(landmarks) != 68 * 2:
         return False
@@ -73,7 +71,7 @@ def are_landmarks_valid(landmarks, image_shape):
 
     return True
 
-# Função para extrair pontos da mão
+# Função que extrai os pontos da mão
 def extract_hand_landmarks(img):
     if img.dtype == np.float32:
         img = (img * 255).astype(np.uint8)
@@ -88,22 +86,23 @@ def extract_hand_landmarks(img):
     else:
         return None
 
-# Função para combinar as predições dos dois modelos
+# Essa função tem o papel de combinar as predições dos dois modelos para poder fazer a classificação conjunta do modelo da face e da mão
 def combine_predictions(face_pred, gesture_pred):
-    # Combine as predições utilizando média ou máxima confiança
+    # Combine as predições utilizando a média, sendo que se aparecer apenas a face seu máximo será de 50% e se aparecer apenas o gesto seu máximo tb será de 50%
     combined_confidences = (face_pred + gesture_pred) / 2
     dominant_class = np.argmax(combined_confidences)
     confidence = combined_confidences[dominant_class] * 100  # Confiança final como porcentagem
     return dominant_class, confidence
 
-# Carregar os modelos e seus pesos
+# Carregar os modelos e seus pesos, sendo bem importante e isso é definido no código de treinamento
 num_classes = 4  # 'Feliz', 'Raiva', 'Surpreso', 'Triste'
 face_model = create_face_model((224, 224, 3), (68 * 2,), num_classes)
 face_model.load_weights('modelo_emocao_face_vgg16_landmarks.weights.h5')
 
-gesture_model = tf.keras.models.load_model('modelo_landmarks_gesto_emocoes_libras.h5')  # Atualize o caminho conforme necessário
+# Atualize se quiser testar cos outros 2 modelos treinados da face em diferentes contextos, o que está setado é o que tem maior taxa de acerto
+gesture_model = tf.keras.models.load_model('modelo_landmarks_gesto_emocoes_libras.h5') 
 
-# Função para capturar a imagem da câmera do navegador usando Streamlit
+# Função usada para capturar a imagem da câmera do navegador usando Streamlit, será tirado uma imagem em tempo real e classificado em seguida
 def classify_realtime_streamlit():
     st.title("Detecção de Emoções e Gestos em Tempo Real")
     uploaded_image = st.camera_input("Capture uma imagem usando a câmera")
@@ -127,21 +126,21 @@ def classify_realtime_streamlit():
 
             face_prediction = face_model.predict([face_region_resized, face_landmarks])
 
-        # Detectar gestos da mão
+        # Detecta os gestos da mão
         hand_landmarks = extract_hand_landmarks(image)
-        gesture_prediction = np.zeros((1, num_classes))  # Previsão padrão
+        gesture_prediction = np.zeros((1, num_classes)) 
 
         if hand_landmarks is not None:
             hand_landmarks = np.expand_dims(hand_landmarks, axis=0)
             gesture_prediction = gesture_model.predict(hand_landmarks)
 
-        # Combinar predições e determinar a classe dominante
+        # Combina as predições e determinar a classe dominante
         dominant_class, confidence = combine_predictions(face_prediction[0], gesture_prediction[0])
 
-        # Exibir o resultado na tela
+        # Exibe o resultado na tela
         label = f"{class_names[dominant_class]}: {confidence:.2f}%"
         st.write(f"Emoção detectada: {label}")
 
-# Iniciar a aplicação Streamlit
+# Inicia a aplicação Streamlit
 if __name__ == '__main__':
     classify_realtime_streamlit()
